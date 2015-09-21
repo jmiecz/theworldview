66285a14e05c92c8aca16d62132dcfbe7a763cef
Josh Mieczkowski <josh.mieczkowski@markit.com>
2015-09-21 00:56:07 -0600

    import clean up

--- TheWorldView/app/src/main/java/com/futurethought/theworldview/MainActivity.java
+++ TheWorldView/app/src/main/java/com/futurethought/theworldview/MainActivity.java
@@ -1,26 +1,12 @@
 package com.futurethought.theworldview;
 
-import android.app.Activity;
+import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 import com.futurethought.theworldview.adapters.CountryAdapter;
 import com.futurethought.theworldview.data.Country;
